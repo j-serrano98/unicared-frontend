56891cae415565c351815ad6e70cbd911644f291
justJS <jserrano.dev@hotmail.com>
2026-01-23 04:10:24 -0400

    sort careers by id

--- src/app/components/registration-step-by-step/SelectCareerForm.jsx
+++ src/app/components/registration-step-by-step/SelectCareerForm.jsx
@@ -30,7 +30,7 @@ export default function SelectCareerForm({ careers }) {
             <h2 className="mb-4 text-xl font-bold text-gray-900">3. Selecciona tu Carrera</h2>
             <form action={formAction} className="h-full">
                 <div className="grid grid-cols-4 gap-4 bg-gray-900 p-8 rounded-xl overflow-auto h-[50vh]">
-                    {careers.map((career) => {
+                    {careers.sort((a, b) => a.id - b.id).slice(0,5).map((career) => {
                         
                         return (
                             <div onClick={() => setSelectedCareer(career.id)} key={career.id} className={`flex-column h-30 text-center items-center rounded-lg  px-4 py-2 border-gray-700 active:bg-gray-800 ${selectedCareer==career.id ? "bg-gray-800 border-3" : "bg-gray-800 hover:bg-gray-700 border"} hover:cursor-pointer`}>
