49ffedfcd6bd84bf11ec5fa3b041c5fe850dbe55
Josue Serrano <jserrano.dev@hotmail.com>
2026-02-11 01:45:29 -0400

    Fix/fedsd 36 fix payload passed to enrollment serializer (#36)
    * added top parameter to home page to fetch top teachers
    
    * Issue fixed. Changed profile image. Added start and end time variables

--- src/app/components/EnrollmentFormUpdate.jsx
+++ src/app/components/EnrollmentFormUpdate.jsx
@@ -51,7 +51,7 @@ export default function EnrollmentFormUpdate({ enrollmentData, teachersData }) {
                             </div>
                             <div className="sm:col-span-3">
                                 <label htmlFor="teacher-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Teacher</label>
-                                <select id="teacher-name" name="teacher" defaultValue={enrollment.teacher ?? ""} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                <select id="teacher-name" name="teacher" defaultValue={enrollment.teacher_uuid ?? ""} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                                     <option value="">Unassigned</option>
                                     {teachersData.map((t) => <option key={t.uuid} value={t.uuid}>{t.name}</option>)}
                                 </select>
