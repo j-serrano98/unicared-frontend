f2d6abbbf4ffdc093e1c96e7ddbae3eead8ee128
justJS <jserrano.dev@hotmail.com>
2026-02-11 15:09:46 -0400

    Changed language of enrollment form and fixed responsive design. Changed opengraph-image

--- src/app/components/GoBackButton.jsx
+++ src/app/components/GoBackButton.jsx
@@ -5,7 +5,7 @@ export default function GoBackButton() {
 
     return (
         <div className="py-3 flex justify-end">
-            <button type="button" onClick={() => router.back()} className="text-fg-brand  text-white bg-gray-900 border border-brand hover:bg-[#ededed] hover:text-gray-900 hover:cursor-pointer focus:ring-4 focus:ring-brand-subtle font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none rounded-lg">Go back</button>
+            <button type="button" onClick={() => router.back()} className="text-fg-brand  text-white bg-gray-900 border border-brand hover:bg-[#ededed] hover:text-gray-900 hover:cursor-pointer focus:ring-4 focus:ring-brand-subtle font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none rounded-lg">Volver atrás</button>
         </div>
     )
 }
