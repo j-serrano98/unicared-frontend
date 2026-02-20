967b41f55cf8cf94faa81f5b54e3daff5eab0276
justJS <jserrano.dev@hotmail.com>
2026-02-20 14:24:23 -0400

    Added sitemap and robots

---
+++ src/app/sitemap.js
@@ -0,0 +1,36 @@
+import fetcher from "./lib/api/fetcher";
+
+export default async function sitemap() {
+    const baseUrl = "https://unicared.tech";
+
+    const teachers = await fetcher(`teachers/`)
+        .then((res) => res.json());
+
+    const teacherUrls = teachers.map((teacher) => ({
+        url: `${baseUrl}/profesores/${teacher.slug}`,
+        lastModified: new Date(),
+        priority: 0.7,
+    }));
+
+    return [
+        {
+        url: baseUrl,
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+        },
+        {
+        url: `${baseUrl}/login`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+        },
+        {
+        url: `${baseUrl}/register`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+        },
+        ...teacherUrls
+    ]
+}
