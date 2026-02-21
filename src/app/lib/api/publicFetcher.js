039118809df34ed8ceedeb494110de5da81e1fd3
justJS <jserrano.dev@hotmail.com>
2026-02-21 02:14:29 -0400

    added temporary console log to check base url

--- src/app/lib/api/publicFetcher.js
+++ src/app/lib/api/publicFetcher.js
@@ -1,17 +1,18 @@
 export default async function publicFetcher(endpoint) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      next: { revalidate: 3600 },
+    console.log("BASE URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}`,
+        {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        next: { revalidate: 3600 },
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(`Public API request failed (${response.status})`);
     }
-  );
 
-  if (!response.ok) {
-    throw new Error(`Public API request failed (${response.status})`);
-  }
-
-  return response.json();
+    return response.json();
 }
